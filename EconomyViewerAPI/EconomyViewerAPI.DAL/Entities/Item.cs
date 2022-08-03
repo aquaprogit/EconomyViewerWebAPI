00ad75feb05ea6cc1730e2c44a874583166d9194
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-04 01:41:15 +0300

    items adding and substractions same elements

--- EconomyViewerAPI/EconomyViewerAPI.DAL/Entities/Item.cs
+++ EconomyViewerAPI/EconomyViewerAPI.DAL/Entities/Item.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace EconomyViewerAPI.DAL.Entities;
+﻿namespace EconomyViewerAPI.DAL.Entities;
 public class Item
 {
+    private int _count = 1;
+    private int _price = 1;
+
     public int Id { get; set; }
     public string Header { get; set; } = string.Empty;
-    public int Count { get; set; }
-    public int Price { get; set; }
+    public int Count
+    {
+        get => _count;
+        set {
+            if (value > 0)
+                _count = value;
+        }
+    }
+    public int Price
+    {
+        get => _price;
+        set {
+            if (value > 0)
+                _price = value;
+        }
+    }
     public string Mod { get; set; } = string.Empty;
 
     public int PriceForOne => Price / Count;
@@ -33,5 +48,18 @@ public class Item
     public override int GetHashCode()
     {
         return HashCode.Combine(Header, PriceForOne, Mod);
+    }
+
+    public static Item operator +(Item first, Item second)
+    {
+        return first.Equals(second) == false
+            ? throw new ArgumentException("Items with different Headers, Mods and PriceForOne can not be added")
+            : new Item(first.Header, first.Count + second.Count, first.Price + second.Price, first.Mod);
+    }
+    public static Item operator -(Item first, Item second)
+    {
+        return first.Equals(second) == false
+            ? throw new ArgumentException("Items with different Headers, Mods and PriceForOne can not be substracted")
+            : new Item(first.Header, first.Count - second.Count, first.Price - second.Price, first.Mod);
     }
 }
