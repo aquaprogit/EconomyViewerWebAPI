6d9dff4213c1c130625519d0b32a55ccd2ffec55
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-04 01:50:25 +0300

    server entity

---
+++ EconomyViewerAPI/EconomyViewerAPI.DAL/Entities/Server.cs
@@ -0,0 +1,13 @@
+﻿namespace EconomyViewerAPI.DAL.Entities;
+public class Server
+{
+    public string Name { get; set; } = string.Empty;
+    public List<Item> Items { get; set; } = new();
+
+    public Server() { }
+    public Server(string name, List<Item> items)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Items = items ?? throw new ArgumentNullException(nameof(items));
+    }
+}
