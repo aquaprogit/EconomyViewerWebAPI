8db2a55e6447b7facfeab147d1c2cd9555234fcf
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-04 00:21:00 +0300

    item's base implementation
    main dat object

---
+++ EconomyViewerAPI/EconomyViewerAPI.DAL/Entities/Item.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EconomyViewerAPI.DAL.Entities;
+public class Item
+{
+    public int Id { get; set; }
+    public string Header { get; set; } = string.Empty;
+    public int Count { get; set; }
+    public int Price { get; set; }
+    public string Mod { get; set; } = string.Empty;
+
+    public int PriceForOne => Price / Count;
+    public Item() { }
+
+    public Item(string header, int count, int price, string mod)
+    {
+        Header = header ?? throw new ArgumentNullException(nameof(header));
+        Count = count;
+        Price = price;
+        Mod = mod ?? throw new ArgumentNullException(nameof(mod));
+    }
+    public override string ToString()
+    {
+        return $"{Header} {Count} шт. - {Price}";
+    }
+    public override bool Equals(object? obj)
+    {
+        return obj is Item item &&
+            item.Header == Header &&
+            item.PriceForOne == PriceForOne &&
+            item.Mod == Mod;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Header, PriceForOne, Mod);
+    }
+}
