7684f955d8494bde338e465e84eceb9dbfe3a7c6
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-04 02:22:06 +0300

    added db context to services

--- EconomyViewerAPI/EconomyViewerAPI/Program.cs
+++ EconomyViewerAPI/EconomyViewerAPI/Program.cs
@@ -1,7 +1,8 @@
+using EconomyViewerAPI.DAL.EF;
+
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDbContext<ApplicationContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
