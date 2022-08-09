42b7dccbbd029bfe1d7b12beeb1827bff15f8984
aquaprogit <vladd.golovatyuk@gmail.com>
2022-08-09 19:19:46 +0300

    disposing of baserepo class

---
+++ EconomyViewerAPI/Backend/EconomyViewerAPI.BLL/Repos/Base/BaseRepo.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EconomyViewerAPI.DAL.EF;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EconomyViewerAPI.BLL.Repos.Base;
+public abstract class BaseRepo<T> : IRepo<T> where T : class
+{
+    private readonly bool _disposeContext;
+    private bool _disposed;
+
+    public ApplicationContext Context { get; }
+    public DbSet<T> Table { get; }
+
+    protected BaseRepo(ApplicationContext context)
+    {
+        Context = context ?? throw new ArgumentNullException(nameof(context));
+        Table = Context.Set<T>();
+        _disposeContext = false;
+    }
+    protected BaseRepo(DbContextOptions<ApplicationContext> options) : this(new ApplicationContext(options))
+    {
+        _disposeContext = true;
+    }
+
+    public int Add(T entity, bool persist = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int AddRange(IEnumerable<T> entities, bool persist = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Delete(int id, byte[] timeStamp, bool persist = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Delete(T entity, bool persist = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int DeleteRange(IEnumerable<T> entities, bool persist = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T? Find(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int SaveChanges()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Update(T entity, bool persist = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int UpdateRange(IEnumerable<T> entities, bool persist = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+            return;
+
+        if (disposing && _disposeContext)
+            Context.Dispose();
+
+        _disposed = true;
+    }
+
+    ~BaseRepo()
+    {
+        Dispose(disposing: false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
--- EconomyViewerAPI/Backend/EconomyViewerAPI.BLL/Repos/Base/IRepo.cs
+++ EconomyViewerAPI/Backend/EconomyViewerAPI.BLL/Repos/Base/IRepo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EconomyViewerAPI.BLL.Repos;
+﻿namespace EconomyViewerAPI.BLL.Repos.Base;
 public interface IRepo<T> : IDisposable
 {
     int Add(T entity, bool persist = true);
